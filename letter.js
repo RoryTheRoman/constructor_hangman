ab1f44d68d03894b50ae6dddc9d859dd63cb9410
Mike D <michael.mcdaniel210@gmail.com>
2018-02-09 19:13:52 -0600

    fixed letter.js, I think

--- letter.js
+++ letter.js
@@ -1,3 +1,18 @@
-function Letter (char, guess,){
-    
-}
+function Letter (char){
+    this.char = char;
+    this.guess = false;
+    this.reveal = function (){
+        if (this.guess === false){
+            return "_";
+        }else{
+            return this.char;
+        };
+    };
+    this.check = function (userInput){
+        if(this.userInput === char){
+            this.guess = true;
+        };       
+    };
+};
+
+module.exports = Letter;
