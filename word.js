9fd72179709afca9ae9c033c10d609dd0c81d5f4
Mike D <michael.mcdaniel210@gmail.com>
2018-02-09 19:35:52 -0600

    constructed word.js

--- word.js
+++ word.js
@@ -1,6 +1,22 @@
 var Letter = require("/.letter.js");
 
 function Word (){
-    this.lettArr = [];
+    var playWords = new Letter ([
+        "whatthefuck", 
+        "holyshit", 
+        "whatamievendoing",
+    ]);
+    this.toString = function(){
+        var newPlayWord = this.PlayWords;
+        reveal();
+        return;
+    }
+    this.userGuess = function(userGuess){
+        check();
+        return;
+    }
+        
+    }
     
-}
+    module.exports = Word;
+   
